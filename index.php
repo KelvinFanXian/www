a2a99af1a5464263eeae823625ca36dc9b523026
KelvinFan <549652152@qq.com>
2020-08-13 01:00:35 +0800

    update

--- index.php
+++ index.php
@@ -1,28 +1,43 @@
 <?php  
 session_start();
+	echo'<link rel="icon" sizes="48x48" href="http://kelvin.vip/favorite.ico">';
+	$type = $_GET['t'];
+#phpinfo();
 	$artis = $_SESSION['artis'];
+	function type_filter($v){
+		return explode('/', $v)[2] == $_GET['t'];
+	}
+	if(!empty($type)){
+		$artis = array_filter($artis, 'type_filter');
+	}
 	if(empty($artis)){
 		require 'pull.php';
 		$artis = $_SESSION['artis'];
 	}
 	// this file
 	$arti = $artis[array_rand($artis)];
-	$textContent = file_get_contents($arti);
-	$textContent = str_replace('"', '&quot;', $textContent);
-	$title = explode('/', $arti);
- ?> 
+	$file = explode('/', $arti)[3];
+	$ext = explode('.',$file)[1];
+	$mdContent = $pdfContent = '';
+	if($ext=='md') {
+		$mdContent = file_get_contents($arti);
+#		$mdContent = str_replace('"', '&quot;', $mdContent);
+	}else if($ext=='pdf'){
+		$pdfContent = $arti;
+	}
+	?> 
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title><?php echo substr($title[3],0,-3) ?></title>
-	<link rel="icon" sizes="48x48" href="http://kelvin.vip/me.ico">
+	<title><?php echo explode('.', $file)[0] ?></title>
+	<link rel="icon" sizes="48x48" href="http://kelvin.vip/favorite.ico">
 </head>
 <style>
 #idx{
 position: fixed;
 top: 5vh;
-right:5vw;
+right:15vw;
 }
 #show{
 border:1px dashed ivory;
@@ -30,12 +45,18 @@ background: transparent;
 }
 </style>
 <body >
-  <input type="hidden" name="textContent" id="textContent" value="<?php echo $textContent; ?>">
-	<div id="content" onmousedown="whichButton(event)" style="padding:10px 64px"></div>
+	<?php if(!empty($mdContent)){
+			require 'Parsedown.php';
+			$Parsedown = new Parsedown();
+			echo '<div id="content" style="padding:10px 64px">'.$Parsedown->text($mdContent).'</div>';
+	} ?>
+
+	<?php if(!empty($pdfContent)) {
+		echo '<embed src="'.$pdfContent.'"  width="100%" height="100%"></embed>';
+	} ?>
 	<div id="idx" style="">
 		<input type="text" id="show"/>
 	</div>
-	<script src="marked.min.js"></script>
 	<script src="js/common.js"></script>
 	<script>
 	  // 关闭右键菜单
@@ -52,7 +73,18 @@ background: transparent;
 				case 13: //Enter
 				 toggleFullScreen();
 				 break;
-				
+
+				// type
+				case 82: //R ruankao
+				 location.href='http://kelvin.vip?t=rk';
+				 break;
+				case 77: //M mysql
+				 location.href='http://kelvin.vip?t=mysql';
+				 break;
+				case 83: //S stock
+				 location.href='http://kelvin.vip?t=stock';
+				 break;
+
 				case 84: //T
 				 window.scroll(0, 0);
 				 break;
@@ -80,7 +112,7 @@ background: transparent;
 				 break;
 			}
 		}
-		document.getElementById('content').innerHTML = marked(document.getElementById("textContent").value);
+
 		// 监听鼠标
 		function whichButton(e){
 			return;
